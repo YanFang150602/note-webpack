b766e9101b233dad3f8f1593749d09783b7c0d54
颜泽芳 <838641570@qq.com>
2023-12-29 15:01:13 +0800

    Create index.js

---
+++ webpack-start-SplitChunk/src/index.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+import { join } from './module.js';
+// import { Button } from '@nce/eview-react';
+
+function component() {
+  const element = document.createElement('div');
+
+  // - lodash（目前通过一个 script 引入）对于执行这一行是必需的
+  // +- lodash 在当前 script 中使用 import 引入
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ', join([1, 2]));
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+// console.log(Button);
