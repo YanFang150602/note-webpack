79d842cf7b0a08b9a0a15b5ac4b9cf400e667e2f
颜泽芳 <838641570@qq.com>
2023-12-29 14:53:51 +0800

    Create webpack.config.js

---
+++ webpack-start/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html'
+    })
+  ]
+};
